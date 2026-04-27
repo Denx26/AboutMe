854193e5a58b354208f9e38ea7cc401bd31b9716
Denx26 <denisiosif26@gmail.com>
2026-04-27 15:15:50 +0300

    Added necessary files

---
+++ script/script.js
@@ -0,0 +1,28 @@
+  if('scrollRestoration' in history){
+        history.scrollRestoration = 'manual';
+    }
+
+document.addEventListener('DOMContentLoaded', () => {
+    window.scrollTo(0, 0);
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('nav a');
+
+    window.addEventListener('scroll', () => {
+        let current = '';
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            
+            if (pageYOffset >= sectionTop - 150) {
+                current = section.getAttribute('id');
+            }
+        });
+
+        navLinks.forEach(link => {
+            link.classList.remove('text-blue-500');
+
+            if (curent && link.getAttribute('href').includes(current)) {
+                link.classList.add('text-blue-500');hghi
+            }
+        });
+    });
+});
