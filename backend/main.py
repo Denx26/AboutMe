8854110bbdf48422aceaf7c23142314ba2248a6f
Denx26 <denisiosif26@gmail.com>
2026-05-02 00:17:21 +0300

    Final production build - Gemini 2.5 Flash

--- backend/main.py
+++ backend/main.py
@@ -20,10 +20,13 @@ load_dotenv()
 
 client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
 
+for models in client.models.list():
+    print(f"Model name: {models.name}")    
+
 try:
-    with open("info-ro/info_denis_ro.json", "r", encoding="utf-8") as f:
+    with open("./info-en/info_denis_en.json", "r", encoding="utf-8") as f:
         denis_info = json.load(f)
-    with open("info-en/info_denis_en.json", "r", encoding="utf-8") as f1:
+    with open("./info-ro/info_denis_ro.json", "r", encoding="utf-8") as f1:
         denis_info1 = json.load(f1)
 except Exception as e:
     print(f"Error loading JSON: {e}")
@@ -38,8 +41,8 @@ async def chat(msg: Message):
         context = denis_info if any(c in msg.message.lower() for c in "ăâîșț") else denis_info1
         context_string = json.dumps(context, ensure_ascii=False)
         
-        response = client.models.generate_content(
-            model="gemini-2.0-flash",
+        response = await client.aio.models.generate_content(
+            model="gemini-2.5-flash",
             contents=msg.message,
             config=types.GenerateContentConfig(
                  system_instruction=(
@@ -53,10 +56,12 @@ async def chat(msg: Message):
                 "4. Daca cineva te intreaba ceva ce nu e in JSON, raspunde bazandu-te pe 'viziunea' ta de asistent AI despre potentialul lui."
                 "5. Daca cineva te intreaba la fel, acelasi lucru sau contexte asemanatoare in English, sa ii raspunzi la fel in English. Fie ca incepe conversatia in English la fel sa continui in English sau daca intreaba ulterior. "                
             ),
-                max_output_tokens=150
+                max_output_tokens=500,
+                temperature=0.8
             )
         )
-        return {"response": response.text}
+        return {"response": response.text if response.text else "Salut! Se pare ca am nevoie o secunda sa ma gandesc. Intreaba-ma iar!"}
     except Exception as e:
         print(f"Eroare API: {e}")
         return {"response": "Momentan am primit prea multe întrebări și mă odihnesc puțin. Reîncearcă în câteva secunde sau contactează-l pe Denis pe email!"}
+    
